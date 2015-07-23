426e1e50800aeb37e832470cca821dd48b6fc372
zero <osmk@qq.com>
2015-07-23 12:14:23 +0800

    Initial Commit

---
+++ GGZipImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GGZipImage
+//
+//  Created by __无邪_ on 15/7/23.
+//  Copyright © 2015年 __无邪_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
